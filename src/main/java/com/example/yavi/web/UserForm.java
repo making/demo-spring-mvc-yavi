dec875b6e24bfd3f078dcc1a904f851644980b82
Toshiaki Maki <tmaki@vmware.com>
2023-05-13 11:36:57 +0900

    Use record for UserForm

--- src/main/java/com/example/yavi/web/UserForm.java
+++ src/main/java/com/example/yavi/web/UserForm.java
@@ -3,37 +3,7 @@ package com.example.yavi.web;
 import am.ik.yavi.core.ValueValidator;
 import com.example.yavi.domain.User;
 
-public class UserForm {
+public record UserForm(String name, String email, Integer age) {
 	static ValueValidator<UserForm, User> validator = User.validator
-			.compose(userForm -> new User(userForm.getName(), userForm.getEmail(), userForm.getAge()));
-
-	private String name;
-
-	private String email;
-
-	private Integer age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+			.compose(userForm -> new User(userForm.name(), userForm.email(), userForm.age()));
 }
