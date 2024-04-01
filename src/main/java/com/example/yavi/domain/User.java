342c98fdf06b529a5a79f502c9deacdb1e109ed9
Toshiaki Maki <makingx@gmail.com>
2024-04-01 12:29:09 +0900

    Apply new style of builder

--- src/main/java/com/example/yavi/domain/User.java
+++ src/main/java/com/example/yavi/domain/User.java
@@ -1,24 +1,25 @@
 package com.example.yavi.domain;
 
-import am.ik.yavi.builder.ValidatorBuilder;
-import am.ik.yavi.core.Validator;
+import am.ik.yavi.arguments.Validator3;
+import am.ik.yavi.validator.Yavi;
 
 import static am.ik.yavi.constraint.charsequence.codepoints.AsciiCodePoints.ASCII_PRINTABLE_CHARS;
 
 public record User(String name, String email, Integer age) {
-	public static final Validator<User> validator = ValidatorBuilder.<User>of()
-			.constraint(User::name, "name", c -> c
+
+	public static final Validator3<String, String, Integer, User> validator = Yavi.validator()
+			._string("name", c -> c
 					.notBlank()
 					.emoji()
 					.lessThanOrEqual(20)
 					.codePoints(ASCII_PRINTABLE_CHARS).asWhiteList())
-			.constraint(User::email, "email", c -> c
+			._string("email", c -> c
 					.notBlank()
 					.lessThanOrEqual(50)
 					.email())
-			.constraint(User::age, "age", c -> c
+			._integer("age", c -> c
 					.notNull()
 					.greaterThanOrEqual(0)
 					.lessThanOrEqual(200))
-			.build();
+			.apply(User::new);
 }
--- src/main/java/com/example/yavi/web/UserForm.java
+++ src/main/java/com/example/yavi/web/UserForm.java
@@ -1,10 +1,10 @@
 package com.example.yavi.web;
 
+import am.ik.yavi.arguments.Arguments;
 import am.ik.yavi.core.ValueValidator;
 import com.example.yavi.domain.User;
 
 public record UserForm(String name, String email, Integer age) {
 	static ValueValidator<UserForm, User> validator = User.validator
-			.applicative()
-			.compose(userForm -> new User(userForm.name(), userForm.email(), userForm.age()));
+			.compose(userForm -> Arguments.of(userForm.name(), userForm.email(), userForm.age()));
 }
