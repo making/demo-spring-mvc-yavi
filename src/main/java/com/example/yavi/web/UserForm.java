46c437e6bc4c3a9ee4c2d2145a3b7ce8aeaa73b3
Toshiaki Maki <makingx@gmail.com>
2024-04-01 12:36:54 +0900

    Add NullAway

--- src/main/java/com/example/yavi/web/UserForm.java
+++ src/main/java/com/example/yavi/web/UserForm.java
@@ -3,8 +3,9 @@ package com.example.yavi.web;
 import am.ik.yavi.arguments.Arguments;
 import am.ik.yavi.core.ValueValidator;
 import com.example.yavi.domain.User;
+import jakarta.annotation.Nullable;
 
-public record UserForm(String name, String email, Integer age) {
+public record UserForm(@Nullable String name, @Nullable String email, @Nullable Integer age) {
 	static ValueValidator<UserForm, User> validator = User.validator
 		.compose(userForm -> Arguments.of(userForm.name(), userForm.email(), userForm.age()));
 
