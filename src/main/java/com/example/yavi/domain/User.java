77114a289125a153e775fb43fa3db72dc0db9d22
Toshiaki Maki <makingx@gmail.com>
2024-04-01 14:40:38 +0900

    Fix

--- src/main/java/com/example/yavi/domain/User.java
+++ src/main/java/com/example/yavi/domain/User.java
@@ -1,13 +1,13 @@
 package com.example.yavi.domain;
 
-import am.ik.yavi.arguments.Validator3;
+import am.ik.yavi.arguments.Arguments3Validator;
 import am.ik.yavi.validator.Yavi;
 
 import static am.ik.yavi.constraint.charsequence.codepoints.AsciiCodePoints.ASCII_PRINTABLE_CHARS;
 
 public record User(String name, String email, Integer age) {
 
-	public static final Validator3<String, String, Integer, User> validator = Yavi.validator()
+	public static final Arguments3Validator<String, String, Integer, User> validator = Yavi.validator()
 		._string("name", c -> c.notBlank().lessThanOrEqual(20).codePoints(ASCII_PRINTABLE_CHARS).asWhiteList())
 		._string("email", c -> c.notBlank().lessThanOrEqual(50).email())
 		._integer("age", c -> c.notNull().greaterThanOrEqual(0).lessThanOrEqual(200))
