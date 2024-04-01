cadf831a28c9119327499eae60688abea3a099a7
Toshiaki Maki <makingx@gmail.com>
2024-04-01 14:55:54 +0900

    Update

--- src/main/java/com/example/yavi/domain/User.java
+++ src/main/java/com/example/yavi/domain/User.java
@@ -7,7 +7,7 @@ import static am.ik.yavi.constraint.charsequence.codepoints.AsciiCodePoints.ASCI
 
 public record User(String name, String email, Integer age) {
 
-	public static final Arguments3Validator<String, String, Integer, User> validator = Yavi.validator()
+	public static final Arguments3Validator<String, String, Integer, User> validator = Yavi.arguments()
 		._string("name", c -> c.notBlank().lessThanOrEqual(20).codePoints(ASCII_PRINTABLE_CHARS).asWhiteList())
 		._string("email", c -> c.notBlank().lessThanOrEqual(50).email())
 		._integer("age", c -> c.notNull().greaterThanOrEqual(0).lessThanOrEqual(200))
